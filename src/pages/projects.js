00593f29327e3c3346094e779e9e9f373eb5113f
phonty29 <amedov.bekmuhamet@gmail.com>
2022-11-06 11:08:17 +0600

    error cause deleted

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -227,8 +227,6 @@ export const pageQuery = graphql`
             tech
             github
             external
-            ios
-            android
             company
           }
           html
